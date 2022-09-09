2cde81a9277e90c5c26d86ecdc681f7548247451
Christian Caed Turner <caed@Christians-MacBook-Pro.local>
2022-09-08 22:14:45 -0700

    Adding Mutiple Circle Markers with formatting.

--- Mapping_Mutiple_Points/static/js/logic.js
+++ Mapping_Mutiple_Points/static/js/logic.js
@@ -4,12 +4,20 @@ console.log("working");
 // create the map object with a center and zoom level
 let map = L.map('mapid').setView([40.7, -94.5], 4);
 
-// Adding a point to the map
-let marker = L.circleMarker([34.0522, -118.2437], {
-    radius: 300,
-    color: 'black',
-    fillColor: '#ffffa1'
-}).addTo(map)
+// Get data from cities.js
+let cityData = cities;
+
+  // Iterate through the cities object and create a marker for each city
+  cityData.forEach(function(city) {
+    console.log(city)
+    L.circleMarker(city.location, {
+      radius: city.population/100000,
+      color: '#FFA500',
+      weight: 4
+    })
+    .bindPopup("<h2>" + city.city + ", " + city.state + "</h2> <hr> <h3> Population " + city.population.toLocaleString() + "</h3>")
+    .addTo(map);
+  });
 
 // We create the tile layer that will be the background of our map
 let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
