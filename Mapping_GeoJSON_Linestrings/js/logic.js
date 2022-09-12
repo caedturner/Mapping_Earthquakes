4c571c4df48d7899d24fbcd8778441d1baa64dd3
Christian Caed Turner <caed@Christians-MacBook-Pro.local>
2022-09-12 12:14:04 -0700

    establish stream.

--- Mapping_GeoJSON_Linestrings/js/logic.js
+++ Mapping_GeoJSON_Linestrings/js/logic.js
@@ -6,7 +6,7 @@ console.log("working");
 
 // We create the tile layer that will be the background of our map
 // We create the dark view tile layer that will be an option for our map.
-let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+let light = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href= "https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
     accessToken: API_KEY
@@ -20,7 +20,7 @@ attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap
 
 // Create a Baselayer that holds both maps.
 let baseMaps = {
-  Street: streets,
+  Light: light,
   Dark: dark
 };
 
