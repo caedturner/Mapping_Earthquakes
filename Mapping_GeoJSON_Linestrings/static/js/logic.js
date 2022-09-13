49f47c3540c327083d9753e9ae4c6e4a3e5ed46b
Christian Caed Turner <caed@Christians-MacBook-Pro.local>
2022-09-12 18:43:12 -0700

    updated with Linestring Example.

--- Mapping_GeoJSON_Linestrings/static/js/logic.js
+++ Mapping_GeoJSON_Linestrings/static/js/logic.js
@@ -6,13 +6,13 @@ console.log("working");
 
 // We create the tile layer that will be the background of our map
 // We create the dark view tile layer that will be an option for our map.
-let light = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+let day = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/navigation-day-v1/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href= "https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
     accessToken: API_KEY
 });
 
-let dark = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+let night = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/navigation-night-v1/tiles/{z}/{x}/{y}?access_token={accessToken}', {
 attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
     accessToken: API_KEY
@@ -20,13 +20,13 @@ attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap
 
 // Create a Baselayer that holds both maps.
 let baseMaps = {
-  Light: light,
-  Dark: dark
+  "Navigation Day": day,
+  "Navigation Night": night
 };
 
 // create the map object with a center and zoom level
 let map = L.map('mapid', {
-  center: [40.7, -94.5],
+  center: [44.0, -80.0],
   zoom: 4
 });
 
@@ -34,18 +34,24 @@ let map = L.map('mapid', {
 L.control.layers(baseMaps).addTo(map);
 
 // Accessing the airport GeoJSON URL
-let airportData = "https://raw.githubusercontent.com/caedturner/Mapping_Earthquakes/main/majorAirports.json"
+let torontoData = "https://raw.githubusercontent.com/caedturner/Mapping_Earthquakes/Mapping_GeoJSON_Linestrings/Mapping_GeoJSON_Linestrings/js/torontoRoutes.json"
+
 
 // Grabbing the GeoJSON data.
-d3.json(airportData).then(function(data) {
+d3.json(torontoData).then(function(data) {
   console.log(data);
 
   //Createing a GeoJSON layer with the retrieved data.
   L.geoJson(data, {
+    color: "#FFFFC2",
+    weight: 2,
     onEachFeature: function(feature, layer) {
-      layer.bindPopup("<h2> Airport Code: " + feature.properties.faa + "</h2> <hr> <h3> Airport Name: " + feature.properties.name + "</h3>");
+      
+      layer.bindPopup("<h2> Airline: " + feature.properties.airline + "</h2> <hr> <h3> Destination: " + feature.properties.dst + "</h3>");
     }
   }).addTo(map);
+  
 });
+
 // Then we add our 'graymap' tile layer to the map
-streets.addTo(map);
+day.addTo(map);
